f194d525af4a44ba3c96cddd24994c63511a70f8
Marcin Borowski <Marcin.Borowski@sabre.com>
2017-01-02 17:59:36 +0100

    Code refactor.

--- src/main/java/com/notifyme/crawlers/custom/TanieLotyCrawler.java
+++ src/main/java/com/notifyme/crawlers/custom/TanieLotyCrawler.java
@@ -2,6 +2,7 @@ package com.notifyme.crawlers.custom;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.notifyme.crawlers.Crawler;
 import com.notifyme.crawlers.HttpGetQueryExecutorWithJsoup;
@@ -9,6 +10,7 @@ import com.notifyme.crawlers.RetrievedData;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 /***
  * @author Marcin Borowski
@@ -28,20 +30,21 @@ public class TanieLotyCrawler implements Crawler
     public List<RetrievedData> getDataByLink(String url, int maxOffersNumber)
     {
         log.debug("Crawler started for url: " + url + " and offer limit: " + maxOffersNumber);
-        List<RetrievedData> offers = new ArrayList<>();
         Document page = new HttpGetQueryExecutorWithJsoup().execute(url);
 
-        page.select("div.eb-post").forEach(offerElement -> {
-            RetrievedData retrievedData = new RetrievedData();
-            retrievedData
-                    .withName(offerElement.select("h2").select("a").get(0).childNode(0).toString())
-                    .withDescription(offerElement.select("div.ebd-block").select("p").get(0).toString())
-                    .withImage(offerElement.select("img").attr("src"))
-            .withUrl("http://" + getDomainAddress() + offerElement.select("h2").select("a").attr("href"));
-            offers.add(retrievedData);
-        });
+        return page.select("div.eb-post").stream()
+                .map(this::retrieveData)
+                .limit(maxOffersNumber)
+                .collect(Collectors.toList());
+    }
 
-        return offers.subList(0,maxOffersNumber);
+    private RetrievedData retrieveData(Element offerElement)
+    {
+        return new RetrievedData()
+                .withName(offerElement.select("h2").select("a").get(0).childNode(0).toString())
+                .withDescription(offerElement.select("div.ebd-block").select("p").get(0).toString())
+                .withImage(offerElement.select("img").attr("src"))
+                .withUrl("http://" + getDomainAddress() + offerElement.select("h2").select("a").attr("href"));
     }
 
     @Override
